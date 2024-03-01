023303d00062f667683daabadab26cf534062c62
Bastian Krol <bastian.krol@web.de>
2024-03-01 12:04:52 +0100

    [dash0] local deployment: remove custom ffs postgres again

--- kubernetes/local/deploy-two-namespaces.sh
+++ kubernetes/local/deploy-two-namespaces.sh
@@ -28,16 +28,16 @@ yq \
   yq --from-file ns2.yq > \
   ns2-values.yaml
 
-helm install --namespace otel-demo-ns1 --create-namespace opentelemetry-demo-postgresql oci://registry-1.docker.io/bitnamicharts/postgresql --values postgres-values.yaml
+# helm install --namespace otel-demo-ns1 --create-namespace opentelemetry-demo-postgresql oci://registry-1.docker.io/bitnamicharts/postgresql --values postgres-values.yaml
 helm install --namespace otel-demo-ns1 --create-namespace opentelemetry-demo-ns1 open-telemetry/opentelemetry-demo --values ns1-values.yaml
 helm install --namespace otel-demo-ns2 --create-namespace opentelemetry-demo-ns2 open-telemetry/opentelemetry-demo --values ns2-values.yaml
 kubectl apply -f cross-namespace-service-names.yaml
-kubectl apply --namespace otel-demo-ns1 -f postgres-service-two-namespaces.yaml
+# kubectl apply --namespace otel-demo-ns1 -f postgres-service-two-namespaces.yaml
 
-kubectl cp ../../src/ffspostgres/init-scripts/10-ffs_schema.sql --namespace otel-demo-ns1 opentelemetry-demo-postgresql-0:/tmp/
-kubectl cp ../../src/ffspostgres/init-scripts/20-ffs_data.sql --namespace otel-demo-ns1 opentelemetry-demo-postgresql-0:/tmp/
-kubectl exec --namespace otel-demo-ns1 opentelemetry-demo-postgresql-0 -- psql postgresql://ffs:ffs@localhost/ffs -a -f /tmp/10-ffs_schema.sql
-kubectl exec --namespace otel-demo-ns1 opentelemetry-demo-postgresql-0 -- psql postgresql://ffs:ffs@localhost/ffs -a -f /tmp/20-ffs_data.sql
+# kubectl cp ../../src/ffspostgres/init-scripts/10-ffs_schema.sql --namespace otel-demo-ns1 opentelemetry-demo-postgresql-0:/tmp/
+# kubectl cp ../../src/ffspostgres/init-scripts/20-ffs_data.sql --namespace otel-demo-ns1 opentelemetry-demo-postgresql-0:/tmp/
+# kubectl exec --namespace otel-demo-ns1 opentelemetry-demo-postgresql-0 -- psql postgresql://ffs:ffs@localhost/ffs -a -f /tmp/10-ffs_schema.sql
+# kubectl exec --namespace otel-demo-ns1 opentelemetry-demo-postgresql-0 -- psql postgresql://ffs:ffs@localhost/ffs -a -f /tmp/20-ffs_data.sql
 
 echo waiting for the frontend and frontendproxy pod to become ready
 kubectl wait --namespace otel-demo-ns-1 --for=condition=ready pod -l app.kubernetes.io/component=frontendproxy --timeout 10s
